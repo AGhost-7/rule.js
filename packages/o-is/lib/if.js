e004dad43f3d6175c9b456b5047b95ff1c56b73f
AGhost-7 <jonathan.boudreau.92@gmail.com>
2016-11-25 00:30:23 +0000

    Remove gitignore

---
+++ packages/o-is/lib/if.js
@@ -0,0 +1,121 @@
+'use strict'
+
+const assign = require('lodash.assign')
+
+const createIfClass = (oIsProto) => {
+
+	function Else(parent, conds, ifTests, tests, boundKeys) {
+		this.parent = parent
+		this.conds = conds
+		this.ifTests = ifTests
+		this.tests = tests
+		this._boundKeys = boundKeys
+	}
+
+	Else.prototype = assign(Object.create(null), oIsProto)
+
+	Else.prototype._create = function(tests, boundKeys) {
+		return new Else(this.parent, this.conds, this.ifTests, tests, boundKeys)
+	}
+
+	Else.prototype.end = function() {
+		return this.parent._cons({
+			type: 'if',
+			conds: this.conds,
+			ifTrue: this.ifTests,
+			ifFalse: this.tests
+		})
+	}
+
+	const Then = function(parent, conds, tests, boundKeys) {
+		this.parent = parent
+		this.conds = conds
+		this.tests = tests
+		this._boundKeys = boundKeys
+	}
+
+	Then.prototype = assign(Object.create(null), oIsProto)
+
+	Then.prototype.end = function() {
+		return this.parent._cons({
+			type: 'if',
+			conds: this.conds,
+			ifTrue: this.tests,
+			ifFalse: []
+		})
+	}
+
+	Then.prototype._create = function(tests, boundKeys) {
+		return new Then(this.parent, this.conds, tests, boundKeys)
+	}
+
+	Then.prototype.else = function() {
+		return new Else(this.parent, this.conds, this.tests, [], this._boundKeys)
+	}
+	const If = function(parent, tests, boundKeys) {
+		this.parent = parent
+		this.tests = tests
+		this._boundKeys = boundKeys
+	}
+
+	If.prototype = assign(Object.create(null), oIsProto)
+	If.prototype.then = function() {
+		return new this._Then(this.parent, this.tests, [], this._boundKeys)
+	}
+	If.prototype._create = function(tests, boundKeys) {
+		return new this._If(this.parent, tests, boundKeys)
+	}
+
+	If.prototype._If = If
+	If.prototype._Then = Then
+	If.prototype._Else = Else
+	Then.prototype._If = If
+	Else.prototype._If = If
+
+	return If
+}
+
+// this will evaluate everything and aggregate the result instead of returning
+// as soon as one of the tests fail.
+const ifAsContinual = (context, tests, self) => {
+	var arr = []
+	for(var i = 0; i < tests.length; i++) {
+		var test = tests[i]
+		var res = self[test.type](context, test, self, true)
+		if(res !== true) {
+			arr.push({
+				context,
+				test,
+				result: res
+			})
+		}
+	}
+	return arr.length === 0 ? true : arr
+}
+
+const ifQuick = (context, tests, self) => {
+	for(var i = 0; i < tests.length; i++) {
+		var test = tests[i]
+		if(!self[test.type](context, test, self, false)) {
+			return false
+		}
+	}
+	return true
+}
+
+const assertIf = (context, args, self, shouldContinue) => {
+	if(!ifQuick(context, args.conds, self)) {
+		if(args.ifFalse.length > 0) {
+			return shouldContinue ?
+				ifAsContinual(context, args.ifFalse, self) :
+				ifQuick(context, args.ifFalse, self)
+		}
+		return true
+	}
+	return shouldContinue ?
+		ifAsContinual(context, args.ifTrue, self) :
+		ifQuick(context, args.ifTrue, self)
+}
+
+module.exports = {assertIf, createIfClass}
+
---
+++ packages/o-is/lib/or.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const assign = require('lodash.assign')
+
+const createOrClass = (oIsProto) => {
+	function Or(parent, tests, boundKeys) {
+		this.parent = parent
+		this._boundKeys = boundKeys
+		this.tests = tests
+	}
+	Or.prototype = assign(Object.create(null), oIsProto)
+	Or.prototype._create = function(tests, boundKeys) {
+		return new Or(this.parent, tests, boundKeys)
+	}
+	Or.prototype.end = function() {
+		return this.parent._cons({
+			type: 'or',
+			tests: this.tests
+		})
+	}
+	// Since by default the chain of tests work like an "and",
+	// I just need to add this option to the Or prototype
+	Or.prototype.and = function() {
+		return new this._And(this, [], this._boundKeys)
+	}
+
+	function And(parent, tests, boundKeys) {
+		this.parent = parent
+		this._boundKeys = boundKeys
+		this.tests = tests
+	}
+	And.prototype = assign(Object.create(null), oIsProto)
+
+	And.prototype._create = function(tests, boundKeys) {
+		return new And(this.parent, tests, boundKeys)
+	}
+	And.prototype.end = function() {
+		return this.parent._cons({
+			type: 'and',
+			tests: this.tests
+		})
+	}
+
+	Or.prototype._And = And
+
+	return Or
+}
+
+const assertOr = (context, args, self) => {
+	for(var i = 0; i < args.tests.length; i++) {
+		var test = args.tests[i]
+		if(self[test.type](context, test, self)) {
+			return true
+		}
+	}
+	return false
+}
+
+const assertAnd = (context, args, self) => {
+	for(var i = 0; i < args.tests.length; i++) {
+		var test = args.tests[i]
+		if(!self[test.type](context, test, self)) {
+			return false
+		}
+	}
+	return true
+}
+
+module.exports = {createOrClass, assertOr, assertAnd}
+
