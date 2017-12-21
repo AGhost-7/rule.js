fdd05bb10801067161efd7d99f06ee4250f11c29
AGhost-7 <jonathan.boudreau.92@gmail.com>
2017-12-20 19:09:30 -0500

    Refactor a bit to make things cleaner

--- packages/o-is-constraint/lib/index.js
+++ packages/o-is-constraint/lib/index.js
@@ -6,75 +6,7 @@ const util = require('util')
 const baseConstraintTypes = require('./base-constraint-types')
 const oIs = require('o-is')
 
-module.exports = function(options) {
-
-	if(!options) options = {}
-
-	const oIsMembers = assign({}, oIs.members, options.members || {})
-	const oIsAssertions = assign({}, oIs.assertions, options.assertions || {})
-	const constraintTypes = assign(
-		{}, baseConstraintTypes, options.constraintTypes || {})
-
-	const constraintMembers = {
-		assert(context) {
-			for(const constraint of this._constraints) {
-				if(oIs.test(oIsAssertions, context, constraint.conditions)) {
-					const errors = this._constraintTypes[constraint.type](
-						context, constraint.args)
-					if(errors.length > 0) {
-						const message = util.format(
-							'Failed validation on field "%s" with rule "%s"',
-							errors[0].key,
-							errors[0].type
-						)
-						throw new Error(message)
-					}
-				}
-			}
-		},
-		errors(context) {
-			const errors = []
-			for(const constraint of this._constraints) {
-				if(oIs.test(oIsAssertions, context, constraint.conditions)) {
-					const constraintType = this._constraintTypes[constraint.type]
-					for(const error of constraintType(context, constraint.args)) {
-						errors.push(error)
-					}
-				}
-			}
-			return errors
-		}
-	}
-
-	for(let type in constraintTypes) {
-		oIsMembers[type] = (function(type) {
-			return function(...args) {
-				return this._constraintBuilder.concat({
-					conditions: this.tests,
-					type: type,
-					args: args
-				})
-			}
-		})(type)
-	}
-
-	for(let key in constraintMembers) {
-		oIsMembers[key] = (function(key) {
-			return function(context) {
-				this._constraintBuilder[key](context)
-			}
-		})(key)
-	}
-
-	const OIsExtended = oIs.createClass(oIsAssertions, oIsMembers)
-
-	const createOIs = OIsExtended.prototype._create
-
-	OIsExtended.prototype._create = function(tests, boundKeys) {
-		const instance = createOIs.call(this, tests, boundKeys)
-		instance._constraintBuilder = this._constraintBuilder
-		return instance
-	}
+const createClass = (constraintMembers, constraintTypes, OIsExtended) => {
 
 	const ConstraintBuilder = function(constraints, constraintTypes) {
 		this._constraints = constraints
@@ -115,6 +47,92 @@ module.exports = function(options) {
 	ConstraintBuilder.prototype.fromJSON = function(constraints) {
 		return new ConstraintBuilder(constraints, constraintTypes)
 	}
+
+	return ConstraintBuilder
+}
+
+const createOIsClass = (
+		oIsMembers, oIsAssertions, constraintMembers, constraintTypes) => {
+
+	for(let type in constraintTypes) {
+		oIsMembers[type] = (function(type) {
+			return function(...args) {
+				return this._constraintBuilder.concat({
+					conditions: this.tests,
+					type: type,
+					args: args
+				})
+			}
+		})(type)
+	}
+
+	for(let key in constraintMembers) {
+		oIsMembers[key] = (function(key) {
+			return function(context) {
+				this._constraintBuilder[key](context)
+			}
+		})(key)
+	}
+
+	const OIsExtended = oIs.createClass(oIsAssertions, oIsMembers)
+
+	const createOIs = OIsExtended.prototype._create
+
+	OIsExtended.prototype._create = function(tests, boundKeys) {
+		const instance = createOIs.call(this, tests, boundKeys)
+		instance._constraintBuilder = this._constraintBuilder
+		return instance
+	}
+
+	return OIsExtended
+
+}
+
+module.exports = function(options) {
+
+	if(!options) options = {}
+
+	const oIsMembers = assign({}, oIs.members, options.members || {})
+	const oIsAssertions = assign({}, oIs.assertions, options.assertions || {})
+	const constraintTypes = assign(
+		{}, baseConstraintTypes, options.constraintTypes || {})
+
+	const constraintMembers = {
+		assert(context) {
+			for(const constraint of this._constraints) {
+				if(oIs.test(oIsAssertions, context, constraint.conditions)) {
+					const errors = this._constraintTypes[constraint.type](
+						context, constraint.args)
+					if(errors.length > 0) {
+						const message = util.format(
+							'Failed validation on field "%s" with rule "%s"',
+							errors[0].key,
+							errors[0].type
+						)
+						throw new Error(message)
+					}
+				}
+			}
+		},
+		errors(context) {
+			const errors = []
+			for(const constraint of this._constraints) {
+				if(oIs.test(oIsAssertions, context, constraint.conditions)) {
+					const constraintType = this._constraintTypes[constraint.type]
+					for(const error of constraintType(context, constraint.args)) {
+						errors.push(error)
+					}
+				}
+			}
+			return errors
+		}
+	}
+
+	const OIsExtended = createOIsClass(
+		oIsMembers, oIsAssertions, constraintMembers, constraintTypes)
+
+	const ConstraintBuilder = createClass(
+		constraintMembers, constraintTypes, OIsExtended)
 
 	return new ConstraintBuilder([], constraintTypes)
 }
