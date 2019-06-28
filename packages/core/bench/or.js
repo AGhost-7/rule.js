e7182dc7d9ebcf6b5ab07fc2bacc2e39e1ad5ef0
AGhost-7 <jonathan.boudreau.92@gmail.com>
2019-06-28 11:29:34 -0400

    improve bench

--- packages/core/bench/or.js
+++ packages/core/bench/or.js
@@ -8,8 +8,15 @@ while (++count < 30) last = last.or()
 last.equal('foo', 'bar')
 while (--count > 0) last = last.end()
 
+const data = [];
+while (++count < 50000) {
+  data.push({ [`foo${count}`]: `foobar${count}` })
+}
+
 const start = Date.now()
-while (++count < 10000) last.test({ foo: 'bar' })
+while (--count > 0) {
+  last.test(data[count])
+}
 const end = Date.now()
 
 console.log('time: %sms', end - start)
